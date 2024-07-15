700e73de5d28290998a195c2b2fa1b778e046283
zhaotianzeng <tianzneg@gmail.com>
2024-07-15 14:42:21 +0800

    增加签名

--- utils/alist/Alist.go
+++ utils/alist/Alist.go
@@ -48,11 +48,40 @@ func GetFileDetail(host string, token string, path string) (GetFileDetailResp, e
 		fmt.Println(err)
 		return resp, err
 	}
-	fmt.Println(string(body))
-
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
 		return resp, err
 	}
 	return resp, nil
+}
+
+func PutFile(host string, token string, path string, file []byte) (GetFileDetailResp, error) {
+	url := host + "/api/fs/put"
+
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", url, bytes.NewReader(file))
+
+	if err != nil {
+		return GetFileDetailResp{}, err
+	}
+	req.Header.Add("Authorization", token)
+	req.Header.Add("File-Path", path)
+	req.Header.Add("User-Agent", "NasKnife/1.0.0")
+	req.Header.Add("As-Task", "false")
+	req.Header.Add("Content-Length", "")
+	req.Header.Add("Content-Type", "text/plain")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return GetFileDetailResp{}, err
+	}
+	defer res.Body.Close()
+
+	_, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return GetFileDetailResp{}, err
+	}
+	return GetFileDetail(host, token, path)
 }
